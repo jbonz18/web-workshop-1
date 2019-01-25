21606150f1aef094949d1199ac6dff9d34f5d159
Josue Wuezo <U91602@ust-global.com>
2019-01-25 09:27:23 -0600

    done

--- data.js
+++ data.js
@@ -2,7 +2,8 @@ var students = [
     {id: "1122222", name: "John", score: 90 }, // 0 {Object} => students[0] => students[0].score => total  
     {id: "2223333", name: "Larry", score: 60 }, // 1
     {id: "4455555", name: "Joseph", score: 50 }, // 2
-    {id: "5526666", name: "Karla", score: 80}
+    {id: "5526666", name: "Karla", score: 80},
+    {id: "1234560", name: "Erika", score: 58}
 ];
 
 document.write("<h3>JSON</h3>");
@@ -31,7 +32,7 @@ function calculateAverage(){
     average = average / students.length;
 
     return average;
-}
+  }
 
 function loadDataGrid() {
 
@@ -54,7 +55,13 @@ function loadDataGrid() {
         var score = document.createElement("div");
         score.classList.add("col-sm");
         score.innerText =  students[i].score;
+                  
+        
+        //score.classList.add("lowscore");
 
+        if(students[i].score <=60) 
+             score.classList.add("lowscore");
+            
         console.log(students[i]);
 
         dataList.appendChild(listItem);
@@ -81,6 +88,7 @@ function displayAverage()
     paragraph.innerText = "Average: " + calculateAverage();
 
     resultSection.appendChild(paragraph);
+
 }
 
 function myReplacer(name, val) {
@@ -112,3 +120,54 @@ function loadData(){
         // i += 3;
     }
 }
+
+function loadData(){
+
+    var i = 0;
+    let dataList = document.getElementById("dataList");
+
+    for (const student of students) {
+        console.log(student.id, student.name, student.score);
+        
+        var listItem = document.createElement("section");
+        listItem.classList.add('row');
+
+        listItem.innerHTML = `<div class="col">${student.id}</div>
+                              <div class="col">${student.name}</div>
+                              <div class="col">${student.score}</div>`;
+
+        dataList.append(listItem);
+    }
+    
+        // while (i < students.length){
+        //     var listItem = document.createElement("li"); //this is also obtained from html ' <li>First</li> '
+            
+        //     console.log(students[i]);
+        //     listItem.innerText = students[i].name;
+
+        //     dataList.appendChild(listItem);
+        //     i = i + 1;  //  or use this ] // Alternatively use 'i++'  
+        //     // i += 2;
+        //     // i += 3;
+        // }
+}
+
+function displayAverage()
+{
+    var resultSection = document.getElementById("resultSection");
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("badge"); //2nd change - Bootstrap classes   
+    paragraph.classList.add("badge-info");  
+
+    paragraph.innerHTML = "Average :" + calculateAverage();
+
+    resultSection.appendChild(paragraph);
+}
+
+function myReplacer(name, val) {
+    if (typeof val === 'string') {
+        return val.toString().toUpperCase();  
+     } else {
+        return val; // return as is
+    }
+};
