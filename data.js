1280fe8c01f3017b615f28a1ec38459b3e42468d
Josue Wuezo <U91602@ust-global.com>
2019-01-28 08:16:46 -0600

    done

--- data.js
+++ data.js
@@ -2,8 +2,7 @@ var students = [
     {id: "1122222", name: "John", score: 90 }, // 0 {Object} => students[0] => students[0].score => total  
     {id: "2223333", name: "Larry", score: 60 }, // 1
     {id: "4455555", name: "Joseph", score: 50 }, // 2
-    {id: "5526666", name: "Karla", score: 80},
-    {id: "1234560", name: "Erika", score: 58}
+    {id: "5526666", name: "Karla", score: 80}
 ];
 
 document.write("<h3>JSON</h3>");
@@ -18,7 +17,7 @@ students.forEach(student => {
 });
 
 document.writeln("Students are:" + names);
-
+//
 document.write("<pre>");
 document.writeln(`Average: ${calculateAverage()}`);
 document.write("</pre>");
@@ -32,13 +31,14 @@ function calculateAverage(){
     average = average / students.length;
 
     return average;
-  }
+}
 
 function loadDataGrid() {
 
     var i = 0;
     let dataList = document.getElementById("dataList");
     
+
     while (i < students.length)
     {
         var listItem = document.createElement("section");
@@ -55,14 +55,8 @@ function loadDataGrid() {
         var score = document.createElement("div");
         score.classList.add("col-sm");
         score.innerText =  students[i].score;
-                  
-        
-        //score.classList.add("lowscore");
 
-        if(students[i].score <=60) 
-             score.classList.add("lowscore");
-            
-        console.log(students[i]);
+        //console.log(students[i]);
 
         dataList.appendChild(listItem);
 
@@ -70,8 +64,15 @@ function loadDataGrid() {
         listItem.appendChild(name);
         listItem.appendChild(score);
 
+        //Paint numbers on red when under the minimun score
+       
+        if (students[i].score < 60){
+            score.style.color = "red";
+        }
+
         i = i + 1; // Alternatively, use i++;
 
+        
         // Other ways:
         // i += 2;
         // i += 3;
@@ -80,15 +81,15 @@ function loadDataGrid() {
 
 function displayAverage()
 {
-    var resultSection = document.getElementById("resultSection");
-    var paragraph = document.createElement("p");
+    //var resultSection = document.getElementById("resultSection");
+    //var paragraph = document.createElement("p");
+    var paragraph = document.getElementById("paragraphAverage")
     paragraph.classList.add("badge"); // 2) Bootstrap classes
     paragraph.classList.add("badge-info");
 
     paragraph.innerText = "Average: " + calculateAverage();
 
     resultSection.appendChild(paragraph);
-
 }
 
 function myReplacer(name, val) {
@@ -121,53 +122,86 @@ function loadData(){
     }
 }
 
-function loadData(){
+//Refresh cu
+function refresh(){
+    //Get minimum score from the user input, on input id "minScore"
+    var minScore = document.getElementById("minScore").value;
+    var i = 0;
+    //console.log(minScore);
+    console.log(students.length);
+    var textTable = document.getElementById("dataList").getElementsByTagName("section");
+    while (i < students.length){
+        
+        var cellScore = textTable[i].childNodes[2];
+        //console.log(cellScore);
+        
+        if (students[i].score < minScore ){
+            cellScore.style.color = "red";
+            }
+            else{
+                cellScore.style.color = "black";
+            }
+        
+        i+=1;
+    }
+}
+
+function addStudent() {
 
     var i = 0;
     let dataList = document.getElementById("dataList");
-
-    for (const student of students) {
-        console.log(student.id, student.name, student.score);
-        
-        var listItem = document.createElement("section");
-        listItem.classList.add('row');
-
-        listItem.innerHTML = `<div class="col">${student.id}</div>
-                              <div class="col">${student.name}</div>
-                              <div class="col">${student.score}</div>`;
-
-        dataList.append(listItem);
-    }
     
-        // while (i < students.length){
-        //     var listItem = document.createElement("li"); //this is also obtained from html ' <li>First</li> '
-            
-        //     console.log(students[i]);
-        //     listItem.innerText = students[i].name;
+    var studentID = document.getElementById("studentID").value;
+    var studentName = document.getElementById("studentName").value;
+    var studentScore = parseInt(document.getElementById("studentScore").value);
 
-        //     dataList.appendChild(listItem);
-        //     i = i + 1;  //  or use this ] // Alternatively use 'i++'  
-        //     // i += 2;
-        //     // i += 3;
+    students.push({id: studentID, name: studentName, score: studentScore});
+    
+    //refresh()
+
+    //loadDataGrid()
+
+    
+
+        var listItem = document.createElement("section");
+        listItem.classList.add("row");
+        
+        var id = document.createElement("div");
+        id.classList.add("col-sm");
+        id.innerHTML =  studentID;
+
+        var name = document.createElement("div");
+        name.classList.add("col-sm");
+        name.innerHTML =  studentName;
+
+        var score = document.createElement("div");
+        score.classList.add("col-sm");
+        score.innerHTML =  studentScore;
+
+        //console.log(students[i]);
+
+        dataList.appendChild(listItem);
+
+        listItem.appendChild(id);
+        listItem.appendChild(name);
+        listItem.appendChild(score);
+
+
+        
+        //students.push({id: id, name: name, score: score});
+        
+
+
+        //Paint numbers on red when under the minimun score
+       
+        // if (score < 60){
+        //     score.style.color = "red";
         // }
-}
 
-function displayAverage()
-{
-    var resultSection = document.getElementById("resultSection");
-    var paragraph = document.createElement("p");
-    paragraph.classList.add("badge"); //2nd change - Bootstrap classes   
-    paragraph.classList.add("badge-info");  
+        // i = i + 1; // Alternatively, use i++;
 
-    paragraph.innerHTML = "Average :" + calculateAverage();
-
-    resultSection.appendChild(paragraph);
-}
-
-function myReplacer(name, val) {
-    if (typeof val === 'string') {
-        return val.toString().toUpperCase();  
-     } else {
-        return val; // return as is
+        
+        // Other ways:
+        // i += 2;
+        // i += 3;
     }
-};
